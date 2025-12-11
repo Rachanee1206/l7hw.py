98aba8b43b837ceadfebf188d5c5c6d0441aa4a7
Rachanee1206 <rachaneeb.t.r1206@gmail.com>
2025-12-11 19:59:41 +0700

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,9 @@
+# check age
+age = int(input("Enter your age:"))
+if age >10:
+    if age>10 and age<=30:
+        print ("you are allowed in sportsclib")
+    else:
+        print("you are not allowed")
+else:
+    print("your too young")
